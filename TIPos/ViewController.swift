7a638ffb8dbf6d9e2fb43af9ec1bf0b34a81f4c2
Amanuel Tesfazgi Tesfamichael <amantesfazgi19@gmail.com>
2020-01-30 20:36:27 -0500

    Initial Commit

---
+++ TIPos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIPos
+//
+//  Created by Amanuel Tesfazgi Tesfamichael on 2020/01/30.
+//  Copyright © 2020 Amanuel Tesfazgi Tesfamichael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
